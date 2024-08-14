780e38a159654c597d458112f63a3c5b0d16306e
itallume <itallo.oliver21@gmail.com>
2024-08-14 02:44:52 -0300

    finalização do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,43 @@
 import Image from "next/image";
 import Logo from "@/image/logo.png"
 import styles from "./Header.module.css"
+import { CiSearch } from "react-icons/ci";
 
 export default function Header(){
 
     return (
     <>
         <header
-        className={styles.Header}>
-            
-            <Image
-            src={Logo}
-            alt="Logo do forum"
-            className={styles.Image}
-            />
-            
-            <input 
-            type="text" 
-            placeholder="Pesquise uma instituição"/>
+        
+        className={styles.header}>
+            <nav
+            className={styles.nav}>
+                <div className={styles.divImage}>
+                    <Image
+                    src={Logo}
+                    alt="Logo do forum"
+                    className={styles.image}
+                    />
+                </div>
+
+                <div className={styles.divSearch}>
+                    <label className={styles.label}>
+                        <CiSearch className={styles.icon}/>
+                        <input 
+                        type="text" 
+                        placeholder="Pesquise uma instituição"
+                        className={styles.input}
+                        />
+                    </label>    
+                </div>
+                <div
+                className={styles.divBotaoLogin}>
+                    <div className={styles.divBotoes}>
+                        <div className={styles.botaoReg}>Registrar-se</div>
+                        <div className={styles.botaoLog}>Entrar</div>
+                    </div>
+                </div>
+            </nav>
         </header>
 
     </>);
