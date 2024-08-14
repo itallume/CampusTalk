9a0b36d1c112435370cbdfb5367c933566c00e03
pauloabaia <paulinhus2@outlook.com>
2024-08-13 22:07:10 -0300

    teste 2 paulo -> itallo

--- src/app/page.js
+++ src/app/page.js
@@ -2,4 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return ();
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+    }}
+    ></div>
 }
