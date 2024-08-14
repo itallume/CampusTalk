493d0ba3ac402c71b34770aab2d9bc6a9da2bfd2
itallume <itallo.oliver21@gmail.com>
2024-08-13 23:45:38 -0300

    troca de cores do header e footer itallo para paulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,23 @@
 import Image from "next/image";
 import Logo from "@/image/logo.png"
+import styles from "./Header.module.css"
+
 export default function Header(){
 
     return (
     <>
-        <header>
+        <header
+        className={styles.Header}>
             
             <Image
             src={Logo}
-            width={500}
-            height={500}
             alt="Logo do forum"
+            className={styles.Image}
             />
             
+            <input 
+            type="text" 
+            placeholder="Pesquise uma instituição"/>
         </header>
 
     </>);
