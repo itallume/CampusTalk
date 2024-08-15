5f370033bee2a7a8a7a02d8a469d170297b296e3
itallume <itallo.oliver21@gmail.com>
2024-08-15 03:32:57 -0300

    prop

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
         </div>
         <div id="feed">
           {posts.map((post) => (<Post id={post.id} username={post.username} postText={post.postText} img={post.img} likes={post.likes} comments={post.comments} pagName={post.pagName}/>))}
-          <Post username="itallo" pagName="IFPB" postText="oiiiiiiiiiiiiiiiiiiiiii" image="https://www.ifpb.edu.br/exposicao-112-anos/fotos/etapa-8/ifpb.jpg/@@images/113992bf-f519-434d-93db-87a789341b83.jpeg" 
+          <Post username="itallo" pagName="IFPB" postText="oiiiiiiiiiiiiiiiiiiiiii" img="https://www.ifpb.edu.br/exposicao-112-anos/fotos/etapa-8/ifpb.jpg/@@images/113992bf-f519-434d-93db-87a789341b83.jpeg" 
         likes="30" comments=" 30"/>
         </div>
         <div id="paiIuToast">
