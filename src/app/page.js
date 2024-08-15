1e1a8ddb0925e6062babc7d62796f452be27706f
pauloabaia <paulinhus2@outlook.com>
2024-08-15 01:33:28 -0300

    do pull

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import TrendingSlider from "@/components/TrendingSlider";
 import Post from "@/components/Post";
 import DropDown from "@/components/DropDown";
+import LoginModal from "@/components/LoginModal";
 
 export default function Home() {
   return (
@@ -12,11 +13,18 @@ export default function Home() {
           <div>
               <TrendingSlider />
           </div>
+
+
+          <div className="flex items-center justify-center h-screen">
+      <LoginModal />
+    </div>
+
+
           <div>
               <DropDown />
           </div>
           <div id="feed">
-              <Post username="itallo" pagName="IFPB"/>
+            <Post username="itallo" pagName="IFPB"/>
           </div>
         
         </section>
